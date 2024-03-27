4e75d3b7b8cd5635ff9856dc57962ff6ce8919f9
hanbit10 <chanbit10@gmail.com>
2024-03-27 10:26:22 +0100

    Main, footer, header update

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,18 +1,50 @@
-let noteTitle = ["Hallo", "Hanbit"];
-let noteText = ["nice to meet you!", "Hanbit"];
+let noteTitle = [
+  "Hallo",
+  "Hanbit",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afddddddddddddddddddddddddddddddddddddd",
+  "afddddddddddddddddddddddddddddddddddddd",
+  "afddddddddddddddddddddddddddddddddddddd",
+];
+let noteText = [
+  "nice to meet you!",
+  "Hanbit",
+  "afdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+  "afdddddddddddddddddddddddddddddddddddddafddddddddddddddddddddddddddddddddddddd",
+];
+
+let menuBar = false;
 
 function render() {
   let note = document.getElementById("content");
   note.innerHTML = "";
 
   for (let i = 0; i < noteTitle.length; i++) {
-    note.innerHTML += /*html*/ `<div class="note-block">${noteTitle[i]} <br> ${noteText[i]} <br>
-  <img class="note-img" src="/src/assets/icons/menu-dot.svg"></div>`;
+    note.innerHTML += /*html*/ `
+    <div id="note-block${i}" class="note-block">
+      <div class="note-text">    ${noteTitle[i]} <br> ${noteText[i]} </div>
+
+    <div onclick="deleteCard(${i})" class="note-cancel">
+      X
+    </div>
+ </div>`;
   }
 }
 
 function addCard() {
   document.getElementById("editCard").classList.remove("d-none");
+}
+
+function deleteCard(index) {
+  let cardID = document.getElementById(`note-block${index}`);
+  cardID.classList.add("d-none");
+  noteTitle.splice(index, 1);
+  noteText.splice(index, 1);
+  render();
 }
 
 function closeEditCard() {
