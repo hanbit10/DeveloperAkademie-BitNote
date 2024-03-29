4b5bf8c1756cdb879e55409b4c4436030dae6711
hanbit10 <chanbit10@gmail.com>
2024-03-29 10:31:58 +0100

    Edit update

--- src/script.js
+++ src/script.js
@@ -36,23 +36,48 @@ function render() {
   getTrash();
   for (let i = 0; i < noteTitle.length; i++) {
     note.innerHTML += /*html*/ `
-    <div id="note-block${i}" class="note-block">
-      <div class="note-text">  <h3>${noteTitle[i]}</h3> <br> ${noteText[i]} </div>
-    <div onclick="deleteCard(${i})" class="note-cancel"> X </div>
- </div>`;
+    <div onclick="editCard(${i})" id="note-block${i}" class="note-block">
+      <div onclick="deleteCard(${i})" class="note-cancel"> x </div>
+      <div class="note-text"> 
+          <h3>${noteTitle[i]}</h3> <br> ${noteText[i]} 
+      </div>
+    </div>
+ 
+      `;
   }
 }
 
-function addCard() {
+function editCard(i) {
   document.getElementById("editCard").classList.remove("d-none");
+  let editTitle = document.getElementById("editCardInput");
+  let editText = document.getElementById("editCardTextArea");
+  editTitle.value = noteTitle[i];
+  editText.value = noteText[i];
+
+  let saveButton = document.getElementById("saveEditCard");
+  saveButton.innerHTML = /*html*/ `
+  <button class="save-btn" onclick="saveEditCard(${i})" > SAVE</button>
+  `;
+}
+
+function addCard() {
+  document.getElementById("createCard").classList.remove("d-none");
+}
+
+function closeCreateCard() {
+  document.getElementById("createCard").classList.add("d-none");
+  let newTitle = document.getElementById("createCardInput");
+  let newText = document.getElementById("createCardTextArea");
+  newTitle.value = "";
+  newText.value = "";
 }
 
 function closeEditCard() {
   document.getElementById("editCard").classList.add("d-none");
-  let newTitle = document.getElementById("editCardInput");
-  let newText = document.getElementById("editCardTextArea");
-  newTitle.value = "";
-  newText.value = "";
+  // let newTitle = document.getElementById("editCardInput");
+  // let newText = document.getElementById("editCardTextArea");
+  // newTitle.value = "";
+  // newText.value = "";
 }
 
 function deleteCard(index) {
@@ -68,15 +93,34 @@ function deleteCard(index) {
   render();
 }
 
-function saveEditCard() {
+function saveCreateCard() {
+  document.getElementById("createCard").classList.add("d-none");
+
+  let newTitle = document.getElementById("createCardInput");
+  let newText = document.getElementById("createCardTextArea");
+
+  if (newTitle.value && newText.value) {
+    noteTitle.push(newTitle.value);
+    noteText.push(newText.value);
+    newTitle.value = "";
+    newText.value = "";
+  } else {
+    alert("fiel out the form");
+  }
+
+  saveCard();
+  render();
+}
+
+function saveEditCard(i) {
   document.getElementById("editCard").classList.add("d-none");
 
   let newTitle = document.getElementById("editCardInput");
   let newText = document.getElementById("editCardTextArea");
 
   if (newTitle.value && newText.value) {
-    noteTitle.push(newTitle.value);
-    noteText.push(newText.value);
+    noteTitle[i] = newTitle.value;
+    noteText[i] = newText.value;
     newTitle.value = "";
     newText.value = "";
   } else {
